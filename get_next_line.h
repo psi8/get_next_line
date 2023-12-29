10cb05e23c80c3b6a55abd1c6a8e632a15dcfee2
Pavel Sitkin <psitkin@c2r6p3.hive.fi>
2023-12-29 22:08:37 +0200

    first header version

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psitkin <psitkin@hive.student.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 21:51:46 by psitkin           #+#    #+#             */
+/*   Updated: 2023/12/29 22:06:51 by psitkin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+# endif
+
+#include <stdib.h>
+#include <unistd.h>
+
+#endif
